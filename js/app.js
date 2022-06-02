967edfc7ac2598802cbd7db8c6f6ee4d8d302ff3
Pierce Petit <ppetit2496@gmail.com>
2022-06-01 19:00:50 -0500

    final.1 commit

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const submitButton = document.querySelector("#submit-button")
 const responceInput = document.querySelector("#To-Do")
 const listBank = document.querySelector("#todo-list")
 
-submitButton.addEventListener('click', function () {
+submitButton.addEventListener('click', function () { // had the hardest time with this function i 100% need to look over eventlisteners some more
     if(responceInput.value === "") return
     const newToDo = document.createElement('li')
     newToDo.textContent = responceInput.value
